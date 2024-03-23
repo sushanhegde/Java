b0558a4055689656f46d3017413fde3026649d09
Hegde <sushan.hegde@accenture.com>
2024-03-23 18:19:30 +0530

    code

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+	public static void main(String args[]) {
+		System.out.println("Today we are learning Jenkin CI and CD tools");
+		System.out.println("We are creating more than one jenkin jobs");
+		String name = "Steven";
+		System.out.println("Welcome user "+name);
+	}
+}
