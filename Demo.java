316556d0c035258bff7dce68cb8f0889878de037
Hegde <sushan.hegde@accenture.com>
2024-03-24 11:31:59 +0530

    changed the program

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
 public class Demo {
 	public static void main(String args[]) {
-		System.out.println("Today we are learning Jenkin CI and CD tools");
-		System.out.println("We are creating more than one jenkin jobs");
-		String name = "Subeen";
-		System.out.println("Welcome user "+name);
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("sum of two numbers="+sum);
 	}
 }
